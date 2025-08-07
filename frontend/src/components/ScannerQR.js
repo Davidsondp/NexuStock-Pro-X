ecf20844d617e1e554c6003446cd043908cf4903
Davidsondp <technologyinte@gmail.com>
2025-08-07 00:04:11 -0300

    Update ScannerQR.js

--- frontend/src/components/ScannerQR.js
+++ frontend/src/components/ScannerQR.js
@@ -1,1 +1,38 @@
+import { Html5QrcodeScanner } from 'html5-qrcode';
 
+export class ProductScanner {
+  constructor(elementId, onScan) {
+    this.scanner = new Html5QrcodeScanner(
+      elementId,
+      {
+        fps: 10,
+        qrbox: { width: 250, height: 250 },
+        formatsToSupport: [
+          Html5QrcodeSupportedFormats.UPC_A,
+          Html5QrcodeSupportedFormats.EAN_13
+        ]
+      },
+      false
+    );
+    this.onScan = onScan;
+  }
+
+  start() {
+    this.scanner.render((decodedText) => {
+      this.onScan(decodedText);
+      this.stop();
+    }, (error) => {
+      console.error("Error al escanear:", error);
+    });
+  }
+
+  stop() {
+    this.scanner.clear();
+  }
+}
+
+// Uso en componente React/Svelte/Vue:
+// const scanner = new ProductScanner('qr-reader', (code) => {
+//   console.log('Código escaneado:', code);
+// });
+// scanner.start();
